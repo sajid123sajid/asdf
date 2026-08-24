4e8a17621e815cf8dc9c70cac5185cc89447e2d8
sajid123sajid <sajedaakter589@gmail.com>
2026-08-24 20:27:52 +0600

    Fix Vite path resolution alias and root imports for Linux Cloudflare build

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,7 +10,7 @@ import {
 import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
-import { reportLovableError } from "../lib/lovable-error-reporting";
+import { reportLovableError } from "@/lib/lovable-error-reporting";
 import { Header } from "@/components/zupona/Header";
 import { CartDrawer } from "@/components/zupona/CartDrawer";
 import { MobileBottomGroup } from "@/components/zupona/MobileBottomGroup";
